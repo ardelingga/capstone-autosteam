b79b85cfdf7f64b81da90b1064e565369b965a79
Ardelingga Pramesta Kusuma <ardelingga.pk@gmail.com>
2024-12-06 07:33:30 +0700

    Change configuration tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
 export default {
   darkMode: ['class'],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -66,5 +67,5 @@ export default {
       sans: ['"Poppins", sans-serif;'],
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [tailwindcssAnimate],
 };
